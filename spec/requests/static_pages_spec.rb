9741dfc9925bdc5596a2749604a617fe29a56481
Chad Jemmett <chad.jemmett@gmail.com>
2013-06-08 20:49:12 -0600

    now have dynamic titles

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,6 +7,11 @@ describe "StaticPages" do
       page.should have_content('sample app')
       page.should have_content('Blergington')
     end
+    it "Should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+      :text => "RoR | Home")
+    end
   end
 
   describe "Help Page" do
@@ -15,12 +20,22 @@ describe "StaticPages" do
       page.should have_content("Help")
       page.should have_content("What the what?!")
     end
+    it "Should have the right title" do
+      visit '/static_pages/help'
+      page.should have_selector('title', 
+      :text => "RoR | Help")
+    end
   end
 
   describe "About page" do
     it "Should have the content 'About me'" do
       visit '/static_pages/about'
       page.should have_content("About Me")
+    end
+    it "Should have the right title" do
+      visit '/static_pages/about'
+      page.should have_selector('title',
+      :text => "RoR | About")
     end
   end
 
