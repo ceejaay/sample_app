0e0151ddfca2094d0c6a5eb6fb0b82a7809769f9
Chad Jemmett <chad.jemmett@gmail.com>
2013-06-08 20:07:27 -0600

    Added an about page, controller and tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,7 +5,23 @@ describe "StaticPages" do
     it "Should have teh content 'sample app' " do
       visit '/static_pages/home'
       page.should have_content('sample app')
+      page.should have_content('Blergington')
     end
-
   end
+
+  describe "Help Page" do
+    it "Should have the content 'help' " do
+      visit '/static_pages/help'
+      page.should have_content("Help")
+      page.should have_content("What the what?!")
+    end
+  end
+
+  describe "About page" do
+    it "Should have the content 'About me'" do
+      visit '/static_pages/about'
+      page.should have_content("About Me")
+    end
+  end
+
 end
