593960c3036358fe47e9cf302ae5b4d6baeae86a
Chad Jemmett <chad.jemmett@gmail.com>
2013-06-11 13:57:49 -0600

    broke the email validation tests. Have to redo them.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,4 +17,23 @@ describe User do
   
   it {should respond_to(:name)}
   it {should respond_to(:email)}
+
+  it {should be_valid}
+
+  describe "when name is not present" do
+    before {@user.name = " "}
+    it {should_not be_valid}
+  end
+  
+  describe "When e-mail is not present" do
+    before{ @user.email = " "}
+    it {should_not be_valid}
+  end
+
+  describe "When name is too long" do
+    before {@user.name = "A" * 51 }
+    it {should_not be_valid}
+  end
+
+#redo the email validations. there was a problem with the vim file.  
 end
