62e272d20e56571ac56120c57091aaf1abaa24b5
Chad Jemmett <chad.jemmett@gmail.com>
2013-07-09 13:48:16 -0600

    added password info to db. added test to check for pw. p.256

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@
 
 class User < ActiveRecord::Base
   attr_accessible :email, :name
+  before_save {|user| user.email = email.downcase }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, presence: true, length: {maximum: 50}
   validates :email, presence: true,
